5771366237f4d7f7b7786de80933712e13548f4f
teospadotto <teospadotto@gmail.com>
2025-12-07 11:37:14 +0100

    feat(bencode): finalize torrent file with info hash

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,14 @@ mod parser;
 
 use std::{fs};
 use crate::parser::bencode::parse_bencode;
+use crate::parser::torrent_file::TorrentFile;
 
 
 fn main() -> std::io::Result<()> {
-    let becode_input = fs::read("/Users/teospadotto/Documents/project/Rust/study/resource/debian-12.10.0-amd64-netinst.iso.torrent")?;
-    let oggetto_deserializzato = parse_bencode(&becode_input);
-
-    println!("{:?}", oggetto_deserializzato.0);
+    let bencode_byte = fs::read("/Users/teospadotto/Documents/project/Rust/study/resource/debian-12.10.0-amd64-netinst.iso.torrent")?;
+    let bencode_input = parse_bencode(&bencode_byte);
+    let torrent= TorrentFile::new_from_bencode(&bencode_input.0);
+    println!("{:?}", torrent.unwrap());
 
     Ok(())
 }
--- src/parser/torrent_file.rs
+++ src/parser/torrent_file.rs
@@ -1,28 +1,103 @@
 use std::io::Read;
 use crate::parser::bencode;
-use crate::parser::bencode::BencodeValue;
+use crate::parser::bencode::{encode_bencode, BencodeValue};
+use sha1::{Digest, Sha1};
+use sha1::digest::typenum::Length;
 
 #[derive(Debug)]
-struct TorrentFile{
-    announce : String
+pub struct TorrentFile{
+    announce : String,
+    name : String,
+    length : i64,
+    piece_length : i64,
+    pieces: Vec<u8>,
+    info_hash : [u8; 20],
 }
 
 impl TorrentFile {
-    pub fn new(announce: String) -> Self {
-        Self { announce }
+    pub fn new(announce: String, name: String, length: i64, piece_length : i64, pieces : Vec<u8>, info_hash : [u8; 20]) -> Self {
+        Self { announce,name,length, piece_length, pieces, info_hash}
     }
 
-    pub fn new_from_bencode(bencode: BencodeValue) -> Result<Self, String> {
-        let announce = Self::find_key_in_bencode(bencode, "announce".to_string());
-        match announce {
-            Err(e) => Err(format!("Unable to find key announce: {}", e)),
-            Ok(url) => {
-                Ok(TorrentFile::new(url))
+    pub fn new_from_bencode(bencode: &BencodeValue) -> Result<Self, String> {
+        let announce = Self::find_key_string_in_bencode(bencode, "announce".to_string())?;
+        let name = Self::find_key_string_in_bencode(bencode, "name".to_string())?;
+        let length = Self::find_key_integer_in_bencode(bencode, "length".to_string())?;
+        let piece_length = Self::find_key_integer_in_bencode(bencode, "piece length".to_string())?;
+        let pieces = Self::find_key_bytes_in_bencode(bencode, "pieces".to_string())?;
+        let bencode_info = Self::find_key_dict_in_bencode(bencode, "info".to_string())?;
+        let info_hash = Self::compute_info_hash(&bencode_info);
+
+        Ok(TorrentFile::new(announce,name,length,piece_length,pieces,info_hash))
+    }
+
+
+
+    fn compute_info_hash(info: &BencodeValue) -> [u8; 20] {
+        let encoded = encode_bencode(info);
+        let mut hasher = Sha1::new();
+        hasher.update(&encoded);
+        let hash = hasher.finalize();
+        hash.into()
+    }
+
+    fn find_key_bytes_in_bencode(input_bencode: &BencodeValue, key: String) -> Result<Vec<u8> ,String> {
+        match input_bencode {
+            BencodeValue::Dictionary(input) => {
+                match input.get(&key.as_bytes().to_vec()) {
+                    None => Err("Key not found".to_string()),
+                    Some(value) => match value {
+                        BencodeValue::String(bytes) => {
+                            Ok(bytes.to_vec())
+                        }
+                        _ => Err("Not a integer in the key".to_string()),
+                    },
+                }
             }
+            _ => Err("Not a dictionary provided".to_string()),
         }
     }
 
-    fn find_key_in_bencode(input_bencode: BencodeValue, key: String) -> Result<String, String> {
+    fn find_key_dict_in_bencode(input_bencode: &BencodeValue, key: String) -> Result<&BencodeValue ,String> {
+        match input_bencode {
+            BencodeValue::Dictionary(input) => {
+                match input.get(&key.as_bytes().to_vec()) {
+                    None => Err("Key not found".to_string()),
+                    Some(value) => match value {
+                        BencodeValue::Dictionary(bytes) => {
+                            Ok(value)
+                        }
+                        _ => Err("Not a integer in the key".to_string()),
+                    },
+                }
+            }
+            _ => Err("Not a dictionary provided".to_string()),
+        }
+    }
+
+    fn find_key_integer_in_bencode(input_bencode: &BencodeValue, key: String) -> Result<i64, String> {
+        match input_bencode {
+            BencodeValue::Dictionary(input) => {
+                match input.get(&key.as_bytes().to_vec()) {
+                    None => Err("Key not found".to_string()),
+                    Some(value) => match value {
+                        BencodeValue::Integer(bytes) => {
+                            let s = String::from_utf8(bytes.clone())
+                                .map_err(|_| "Invalid UTF-8 bytes".to_string())?;
+
+                            s.parse::<i64>()
+                                .map_err(|_| "Invalid integer format".to_string())
+                        }
+                        _ => Err("Not a integer in the key".to_string()),
+                    },
+                }
+            }
+            _ => Err("Not a dictionary provided".to_string()),
+        }
+    }
+
+
+    fn find_key_string_in_bencode(input_bencode: &BencodeValue, key: String) -> Result<String, String> {
         match input_bencode {
             BencodeValue::Dictionary(input) => {
                 match input.get(&key.as_bytes().to_vec()) {
@@ -39,8 +114,9 @@ impl TorrentFile {
         }
     }
 
-
 }
+
+
 
 
 #[cfg(test)]
@@ -49,22 +125,34 @@ mod tests {
     use super::*;
 
     #[test]
-    fn find_key_in_bencode() {
+    fn find_string_in_bencode() {
         let mut input_map = HashMap::new();
         input_map.insert("announce".as_bytes().to_vec(),BencodeValue::String("url".as_bytes().to_vec()));
         let input_bencode = BencodeValue::Dictionary(input_map);
 
-        let result = TorrentFile::find_key_in_bencode(input_bencode, "announce".to_string());
+        let result = TorrentFile::find_key_string_in_bencode(&input_bencode, "announce".to_string());
         assert!(result.is_ok());
         assert_eq!(result.unwrap(), "url");
     }
+
+    #[test]
+    fn find_integer_in_bencode() {
+        let mut input_map = HashMap::new();
+        input_map.insert("length".as_bytes().to_vec(),BencodeValue::Integer("54".as_bytes().to_vec()));
+        let input_bencode = BencodeValue::Dictionary(input_map);
+        let result = TorrentFile::find_key_integer_in_bencode(&input_bencode, "length".to_string());
+        assert!(result.is_ok());
+        assert_eq!(result.unwrap(), 54);
+    }
+
 
     #[test]
     fn create_torrent_from_bencode() {
         let mut input_map = HashMap::new();
         input_map.insert("announce".as_bytes().to_vec(),BencodeValue::String("url".as_bytes().to_vec()));
         let input_bencode = BencodeValue::Dictionary(input_map);
-        let result = TorrentFile::new_from_bencode(input_bencode);
-        println!("{:?}", result)
-            }
+        let result = TorrentFile::new_from_bencode(&input_bencode);
+        assert!(result.is_ok());
+        assert_eq!(result.unwrap().announce, "url");
+    }
 }
