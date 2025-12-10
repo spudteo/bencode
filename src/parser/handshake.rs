f64b25d55d762583ea5801395161e124978e2e61
teospadotto <teospadotto@gmail.com>
2025-12-10 14:42:50 +0100

    feat(handshake): added hadnshake object and parsing

---
+++ src/parser/handshake.rs
@@ -0,0 +1,57 @@
+struct Handashake {
+    pstrlen : u8,
+    pstr : [u8; 19],
+    reserved:[u8; 8],
+    info_hash: [u8; 20],
+    peer_id:[u8; 20]
+}
+
+// handshake: <pstrlen><pstr><reserved><info_hash><peer_id>
+//1:19:8:20:20
+
+impl Handashake {
+    fn new(info_hash: [u8; 20],peer_id:[u8; 20]) -> Self {
+        let p = *b"BitTorrent protocol";
+        let plen = p.len() as u8;
+        Self {pstrlen : plen , pstr : p,reserved: [0; 8],info_hash,peer_id }
+    }
+
+    pub fn parse(input_bytes : [u8;68]) -> Self {
+        let pstrlen = input_bytes[0];
+        let mut pstr = [0u8; 19];
+        pstr.copy_from_slice(&input_bytes[1..20]);
+        let mut reserved = [0u8; 8];
+        reserved.copy_from_slice(&input_bytes[20..29]);
+        let mut info_hash = [0u8; 20];
+        info_hash.copy_from_slice(&input_bytes[29..50]);
+        let mut peer_id = [0u8; 20];
+        peer_id.copy_from_slice(&input_bytes[50..71]);
+        Self{pstrlen,pstr,reserved,info_hash,peer_id}
+    }
+
+    pub fn to_bytes(self : Self)  -> [u8;68] {
+        let mut out = [0u8; 68];
+        let mut pos = 1;
+        out[0] = self.pstrlen;
+        out[pos..pos+self.pstr.len()].copy_from_slice(&self.pstr);
+        pos += self.pstr.len();
+        out[pos..pos+self.reserved.len()].copy_from_slice(&self.reserved);
+        pos += self.reserved.len();
+        out[pos..pos+self.info_hash.len()].copy_from_slice(&self.info_hash);
+        pos += self.info_hash.len();
+        out[pos..pos+self.peer_id.len()].copy_from_slice(&self.peer_id);
+        pos += self.peer_id.len();
+        out
+    }
+}
+
+
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
+//
+//     #[test]
+//     fn bencode_parse_integer() {
+//
+//     }
+// }
